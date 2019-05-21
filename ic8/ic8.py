4941f6b02264cd0464026bde028a1ba53cfa2737
ajuluc <ajulu.c@husky.neu.edu>
2019-05-21 10:59:44 -0400

    Added terminationRateRatio=0.01,

--- ic8/ic8.py
+++ ic8/ic8.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:13c1016a422d85aa021ba6b2f70cbd9e1522176dd8157ae45c0715f1d5c352c5
-size 4019
+oid sha256:2ff0423972bc440806550c4d593b3099e9136bea4eff542ba367c7d6f5478bdc
+size 4083
