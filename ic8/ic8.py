b92f15344817fe05a3a434a0eec1ebc888d6c415
ajuluc <ajulu.c@husky.neu.edu>
2019-05-21 10:55:33 -0400

    Changed input species for iC8H18
    For combustion of iso-octane.
    
    Fuel, O2, N2, etc.
    And intermediates that were measured in the JSR experiments.

--- ic8/ic8.py
+++ ic8/ic8.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:96ba628a3398761ea06f62f49667bd64683de202449655c33d03657205d0e132
-size 3618
+oid sha256:dafb55ab2c2993a79cdf395b3888dc1ad579dc0861a7f338ae9ad71f1fd54552
+size 3998
