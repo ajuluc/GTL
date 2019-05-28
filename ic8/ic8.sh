c05d83e0ca9779babda71a1d8ce90f54bed269cb
ajuluc <ajulu.c@husky.neu.edu>
2019-05-28 16:52:58 -0400

    Updated submit script ic8
    Updated the bash shell submit script for ic8 with exclusive and node c3040

--- ic8/ic8.sh
+++ ic8/ic8.sh
@@ -7,7 +7,9 @@
 #SBATCH --partition=west
 #SBATCH --mem=120Gb 
 #SBATCH --exclude=c[5003]
+#SBATCH --nodelist=c[3040]
 
+#SBATCH --exclusive 
 
 source activate rmg_env
 python $RMGpy/rmg.py -p ic8.py
