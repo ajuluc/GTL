8e880916df365ad0dcbf6fbfcd2fd6f8f73e868a
ajuluc <ajulu.c@husky.neu.edu>
2019-05-28 11:52:03 -0400

    Updated input file for iso-octane
    updated input file for iso-octane. Nsims was changed as well as the mole fractions. Libraries are were changed a bit to account for more dependencies in the kinetics as well as thermodynamics

--- ic8/ic8.py
+++ ic8/ic8.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:1013590e0b366b7f480c774e601964deabd589993ab828aa47d13c64b16249dc
-size 4180
+oid sha256:657893de3b619fcf1ed74ab5e83ca242f9ccd191681bd656fb75b11c87052f3e
+size 4197
