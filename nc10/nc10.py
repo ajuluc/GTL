dda5bf83602d1c2884ecd78b153064a2f4ef71e8
ajuluc <ajulu.c@husky.neu.edu>
2019-07-08 16:04:21 -0400

    Added Pruning
    Changed the toleranceKeepInEdge to 0.0055. which is between 0.01 and 0.001 of the toleranceMoveToCore value of 0.1

--- nc10/nc10.py
+++ nc10/nc10.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:c81d6bd2fe460b55bf6125232dc98a56807971b6cff72006d4be10ba01f2280d
-size 4200
+oid sha256:c6c96bda8ddfe2b1fdc41901b28d359f42eb894cd4648254a680466bfed71eb7
+size 4203
