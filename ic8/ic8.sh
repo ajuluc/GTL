a34d78f095e06403dc5f7069a033d57f245ee802
ajuluc <ajulu.c@husky.neu.edu>
2019-07-16 15:19:09 -0400

    Submit script for the new iso-octane file with staging and maxNumSpecies

--- ic8/ic8.sh
+++ ic8/ic8.sh
@@ -2,14 +2,15 @@
 
 #SBATCH --nodes=1
 #SBATCH --time=unlimited
+#SBATCH --mem=120Gb
 #SBATCH --job-name=IC8
 #SBATCH --output=IC8.log
 #SBATCH --partition=west
-#SBATCH --mem=120Gb 
 #SBATCH --exclude=c[5003]
-#SBATCH --nodelist=c[3040]
+#SBATCH --cpus-per-task=6
+#SBATCH --mem-per-cpu=4Gb
+#SBATCH --ntasks=1
 
-#SBATCH --exclusive 
 
 source activate rmg_env
 python $RMGpy/rmg.py -p ic8.py
