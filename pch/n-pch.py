1f1be9dcd41b8dd6fdaf6e2c54b32b34966c5042
ajuluc <ajulu.c@husky.neu.edu>
2019-07-25 09:18:28 -0400

    The input file was updated with staging and maxNumSpecies for n-pch

--- pch/n-pch.py
+++ pch/n-pch.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:a1d2aafadde029a10ca8e7c4ddad4988f7a061fa3c92fde8d244649a4641c429
-size 4190
+oid sha256:716f2ba8bdf5a90471d5421ca8e0a50ace1e81c6946f0f4a91726baf5bbfdf29
+size 4954
