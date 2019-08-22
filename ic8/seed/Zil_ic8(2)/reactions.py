4b022a36cf9286a5949355c487435da2048beca5
ajuluc <ajulu.c@husky.neu.edu>
2019-08-22 17:25:51 -0400

    The run from the interactive node for iso-octane with Atef species identified on the importer included in the input file

---
+++ ic8/seed/Zil_ic8(2)/reactions.py
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:e3ce8b75e6a287889d2ba538dc7760f21d2132bea645afbadf1123915ef63282
+size 4876992
---
+++ ic8/seed/Zil_ic8(2)_edge/reactions.py
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:b26c22b1cd5412dd1970cad4e6015744134b446d42cc02048801256b85f974c8
+size 5047867
