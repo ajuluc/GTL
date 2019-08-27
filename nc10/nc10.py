cdf1dbb3cd85061ba1975215d036bd3693b4a70f
ajuluc <ajulu.c@husky.neu.edu>
2019-08-27 14:42:01 -0400

    removed the duplicate specie C5H10

--- nc10/nc10.py
+++ nc10/nc10.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:205ca553528d9fd442ced7a987a3349832ee731f6874e0993b4bc094220d44dc
-size 4968
+oid sha256:49fbc06ee7deb2c98ad29f1b1fe272b49ced4b2d732898900a343969de04fb9b
+size 19252
