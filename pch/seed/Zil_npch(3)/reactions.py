e57983862d051f8b2cf2739dae0f5910e8cf1833
ajuluc <ajulu.c@husky.neu.edu>
2019-09-15 20:33:01 -0400

    The log file for the non-pdep n-decane file

--- pch/seed/Zil_npch(3)/reactions.py
+++ pch/seed/Zil_npch(3)/reactions.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:507f2f9191beca70fb00e65778d761402e4ad232bbaac76c6a0bced249e6173e
-size 16192321
+oid sha256:66135cfeae22179d0611d5cc790130ca6d49b5cacb6c7183e0d10f23d1a84d14
+size 16211349
