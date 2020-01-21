159249f431938e766382d969cfe0e26b9cbd7168
ajuluc <ajulu.c@husky.neu.edu>
2020-01-21 15:32:25 -0500

    The same nc10 job but without the low temp pathway done over christmas

---
+++ nc10/nc10_lowT/no_lowT/seed/seed/reactions.py
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:89b18740bab6ef70823bc359deffb8ee939570ff166948cc6eb804f0688be04c
+size 4195129
---
+++ nc10/nc10_lowT/no_lowT/seed/seed_edge/reactions.py
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:9d58e4094a2f266b4298c469e9d19845cc79ad2f1ba47c9e4f2f155cfec808f4
+size 148586574
